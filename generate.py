d624a2f2840483f3435723829e76f4242b6cd41b
Zachery Fogg <z.fogg1234@gmail.com>
2021-02-11 19:09:21 -0500

    Completed Torso BackLeg FrontLeg

--- generate.py
+++ generate.py
@@ -19,16 +19,21 @@ def Create_Robot():
     width = 1
     length = 1
     height = 1
-    x = 0
+    x = 1.5
     y = 0
-    z = .5
+    z = 1.5
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[
                       length, width, height])
-    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso",
-                       child="Leg", type="revolute", position="0.5 0 1.0")
-    pyrosim.Send_Cube(name="Leg", pos=[.5, y, .5], size=[
+    pyrosim.Send_Joint(name="Torso_Front_Leg", parent="Torso",
+                       child="FrontLeg", type="revolute", position="2 0 1.0")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[.5, y, -.5], size=[
                       length, width, height])
+    pyrosim.Send_Joint(name="Torso_Back_Leg", parent="Torso",
+                       child="BackLeg", type="revolute", position="1 0 1.0")
+    pyrosim.Send_Cube(name="BackLeg", pos=[-.5, y, -.5], size=[
+                      length, width, height])
+
     pyrosim.End()
 
 
