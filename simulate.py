5ab0e4798abcfbd93ca58c22fb238e713456e182
Zachery Fogg <z.fogg1234@gmail.com>
2021-02-17 15:51:42 -0500

    Added Sensor to Backleg, export numpy array to data folder

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time
 import pybullet_data
+import numpy
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -9,8 +11,15 @@ p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
 bodyId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate("body.urdf")
+backLegSensorValues = numpy.zeros(1000)
+
+
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(1/60)
-
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    backLegSensorValues[i] = backLegTouch
+    time.sleep(1 / 60)
+with open('./data/backLegSensorValues.npy', 'wb') as f:
+    numpy.save(f, backLegSensorValues)
 p.disconnect()
